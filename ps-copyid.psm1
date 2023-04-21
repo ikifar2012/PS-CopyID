6da42c80c320a57fea9a8e659b4b38b6436a1f10
Matheson Steplock <ikifar2012@users.noreply.github.com>
2023-04-21 01:35:54 -0400

    Fix file permission issues

--- ps-copyid.psm1
+++ ps-copyid.psm1
@@ -5,10 +5,10 @@ function Copy-ID {
         $id= $null
     )
     if ($null -eq $id) {
-    $sshdir= "$env:USERPROFILE\.ssh"
-    $rsa= $(Test-Path -Path $sshdir\id_rsa.pub)
-    $ecdsa= $(Test-Path -Path $sshdir\id_ecdsa.pub)
-    $ed25519= $(Test-Path -Path $sshdir\id_ed25519.pub)
+    $sshdir="$env:USERPROFILE\.ssh"
+    $rsa=$(Test-Path -Path "$sshdir\id_rsa.pub")
+    $ecdsa=$(Test-Path -Path "$sshdir\id_ecdsa.pub")
+    $ed25519=$(Test-Path -Path "$sshdir\id_ed25519.pub")
     Write-Output "Testing Keys..."
     if ($ed25519 -eq "True") {
         Write-Output "Found ED25519 Key, Installing..."
@@ -26,6 +26,6 @@ function Copy-ID {
     } else {
         Write-Output "Installing Key..."
     }
-    Get-Content $id | ssh $hostname "mkdir .ssh 2>/dev/null; touch .ssh/authorized_keys 2>/dev/null; cat >> .ssh/authorized_keys"
+    Get-Content $id | ssh $hostname "mkdir .ssh 2>/dev/null; chmod 700 .ssh 2>/dev/null; touch .ssh/authorized_keys 2>/dev/null; chmod 644 .ssh/authorized_keys 2>/dev/null; cat >> .ssh/authorized_keys"
 }
 Export-ModuleMember -Function * -Alias *
